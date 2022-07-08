44a005f65e3d193385e5f96b9fd88e79f3dff982
nemestniy <tushkevich.ma@gmail.com>
2022-07-08 19:17:04 +0300

    Rotation with back moving

--- Assets/RotationWithBackMoving.cs
+++ Assets/RotationWithBackMoving.cs
@@ -6,33 +6,47 @@ using UnityEngine.UI;
 public class RotationWithBackMoving : MonoBehaviour
 {
     public GameObject Pult;
-    public float PultRotationSpeed = 1.0f;
+    public float PultRotationSpeed = 5.0f;
     public float PultBackRotationSpeed = 2.0f;
     public Quaternion StartPosition;
     public Quaternion EndPosition;
     public float AngleRotation = 90.0f;
     public Quaternion OffsetPultRotation;
     public Quaternion DeltaOffsetPultRotation;
+    public float time;
 
-    private bool isNotRotateble = false;
-    private bool isRotateble = false;
+    private bool isRotationStarted = false;
     private Coroutine pultRotationRout;
-    private Coroutine pultBackRotationRout;
+   
     
     void Start()
-        {
-            StartPosition = transform.rotation;
-            EndPosition = StartPosition * Quaternion.Euler(StartPosition.x, StartPosition.y, AngleRotation); 
-            OffsetPultRotation = EndPosition * Quaternion.Euler(EndPosition.x, EndPosition.y, - AngleRotation);            
-        }
+    {
+        time = 0.0f;
+        StartPosition = Pult.transform.rotation;
+        EndPosition = StartPosition * Quaternion.Euler(0.0f, 0.0f, AngleRotation); 
+                  
+    }
     private void Update()
     {
-        if (isNotRotateble)
+        if (Pult != null && isRotationStarted)
         {
-            //pultBackRotationRout = StartCoroutine(pultBackRotationRoutine(EndPosition, StartPosition));
-
-            Pult.transform.rotation = Quaternion.Lerp(EndPosition, StartPosition, Time.deltaTime * PultBackRotationSpeed);
+            time += Time.deltaTime;
+            if (time <= 1.0f)
+            {                
+                Pult.transform.rotation = Quaternion.Lerp(StartPosition, EndPosition, time);
+            }
+            else if(time > 1.0f)
+            {
+                Pult.transform.rotation = Quaternion.Lerp(EndPosition, StartPosition, time - 1.0f);
+            }
+            if (time >= 2.0f)
+            {
+                isRotationStarted = false;
+                time = 0.0f;
+            }
         }
+            
+                            
     }
 
     private IEnumerator pultRotationRoutine(Quaternion startPosition, Quaternion endPosition)
@@ -44,43 +58,25 @@ public class RotationWithBackMoving : MonoBehaviour
             Pult.transform.rotation = Quaternion.Lerp(startPosition, endPosition, time);
             yield return new WaitForEndOfFrame();
         }
-        pultRotationRout = null;
-        isNotRotateble = true; 
-    }
-    private IEnumerator pultBackRotationRoutine(Quaternion startPosition, Quaternion endPosition)
-    {
-        float time = 0.0f;
+        time = 0.0f;
         while (time < 1)
         {
             time += Time.deltaTime * PultBackRotationSpeed;
-            Pult.transform.rotation = Quaternion.Lerp(startPosition, endPosition, time);
+            Pult.transform.rotation = Quaternion.Lerp(endPosition, startPosition, time);
             yield return new WaitForEndOfFrame();
         }
-        pultBackRotationRout = null;
-        isRotateble = true;
+
+        pultRotationRout = null;        
     }
-
-
+    
     public void StartOnClickRoutine()
     {
-        if (Pult != null && isRotateble)
+        if (Pult != null)
         {
-            pultRotationRout = StartCoroutine(pultRotationRoutine(StartPosition, EndPosition));
+            isRotationStarted = true;
+            //pultRotationRout = StartCoroutine(pultRotationRoutine(StartPosition, EndPosition));
         }
-        //if (isNotRotateble)
-        //{
-        //    //pultBackRotationRout = StartCoroutine(pultBackRotationRoutine(EndPosition, StartPosition));
-            
-        //    Pult.transform.rotation = Quaternion.Lerp(EndPosition, StartPosition, Time.deltaTime * PultBackRotationSpeed);
-        //}
-
+        
     }
-
-    public void StartOnBackRoutine()
-    {
-        if (isNotRotateble)
-                {
-                    pultBackRotationRout = StartCoroutine(pultBackRotationRoutine(EndPosition,StartPosition));
-                }
-    }
+    
 }
